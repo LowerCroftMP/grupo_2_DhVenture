5a5d421e2c924010fb5aeb3ab34680277c8fd37f
rika-tula <ricazabala04@gamil.com>
2024-01-28 22:47:53 -0300

    terminé la extructura de archivos

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const port = 3030
+
+const app = express();
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/home.html'))
+})
+
+app.get('/registro', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/register.html'))
+})
+
+app.get('/acceso', (req, res) =>{
+    res.sendFile(path.join(__dirname,'./views/login.html'))
+})
+app.listen(port,() =>console.log(`http://localhost:${port}`))
+
