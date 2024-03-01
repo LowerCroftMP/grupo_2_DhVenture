a67d65ef3aff1f5bfe1cadbb32bcb8a69bec51ce
rika-tula <ricazabala04@gamil.com>
2024-02-29 22:02:35 -0300

    corregi errores del merge

--- routers/admin.routes.js
+++ routers/admin.routes.js
@@ -5,7 +5,6 @@ const router = express.Router()
 const admin= require("../controllers/admin")
 
 router.get("/crear-producto",admin.create)
-
 router.get('/form-edit',admin.form)
 
 module.exports = router
--- routers/other.routes.js
+++ routers/other.routes.js
@@ -10,14 +10,8 @@ router.get('/detalle', controller.detalle)
 router.get('/home/registro', controller.registro)
 router.get('/carrito', controller.carrito)
 
-<<<<<<< HEAD
-router.get('/form-edit',admin.form)
-
 router.get('/dashboard', (req, res) => {
     res.render('dashboard', { pageTitle: 'Dashboard' });
 });
 
-
-=======
->>>>>>> 8375136a5d06b8ffd9fcc63ba5eb9386f39c8062
 module.exports = router
