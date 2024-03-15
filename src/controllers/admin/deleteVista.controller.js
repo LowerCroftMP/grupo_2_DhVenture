01269ec354c352d62cb7ba39dde9118677759782
rika-tula <ricazabala04@gamil.com>
2024-03-15 01:35:58 -0300

    corregí errores del merge

--- src/controllers/admin/deleteVista.controller.js
+++ src/controllers/admin/deleteVista.controller.js
@@ -1,9 +1,5 @@
 module.exports = (req, res) => {
     const { id, title } = req.query;
 
-    res.render('./admin/deleteProduct', { id, title }, (err, contentView) => {
-        err && res.send(send.message)
-
-       res.render('./partials/dashboard', { contentView })
-    })
+    res.render('./admin/deleteProduct', { id, title })
 }
