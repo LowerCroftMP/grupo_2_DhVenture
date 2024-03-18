4a3e6ea0b62326e0f05b88cd89485945aa24f34b
rika-tula <ricazabala04@gamil.com>
2024-03-18 20:50:15 -0300

    errores de delete

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 var createError = require('http-errors')
-
 const express = require('express')
 const path = require('path')
-
 var cookieParser = require('cookie-parser')
-
 var logger = require('morgan')
+const methodOverride =  require('method-override');
 
 
 //*RUTAS
@@ -28,6 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../public')));
+app.use(methodOverride('_method'));
 
 //*ENRUTADORES
 app.use('/', otherRoutes)
--- src/controllers/admin/deleteVista.controller.js
+++ src/controllers/admin/deleteVista.controller.js
@@ -1,4 +1,10 @@
+const { readData } = require("../../data");
+
 module.exports = (req, res) => {
     const { id, name } = req.query;
+    const products = readData();
+
+    const productDestroy = products.find(p => p.id === +id)
+
     res.render('./admin/deleteProduct', { id, name })
 }
