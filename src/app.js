4e52dc2084792e6308ac5c3b4daedac123eb63b6
rika-tula <ricazabala04@gamil.com>
2024-03-18 20:05:58 -0300

    descomente el mensaje de error

--- src/app.js
+++ src/app.js
@@ -36,27 +36,27 @@ app.use('/', cartRoutes)
 app.use('/productos', productsRoutes)
 app.use('/admin', adminRoutes)
 
-//
-//app.use((req,res, next) => {
-//    res.status(404).render("./other/notFound") 
-//  })
-//
 
-//
-//// catch 404 and forward to error handler
-//app.use(function(req, res, next) {
-//  next(createError(404));
-//});
-//
-//// error handler
-//app.use(function(err, req, res, next) {
-//  // set locals, only providing error in development
-//  res.locals.message = err.message;
-//  res.locals.error = req.app.get('env') === 'development' ? err : {};
-//
-//  // render the error page
-//  res.status(err.status || 500);
-//  res.render('./error');
-//});
-//
+app.use((req,res, next) => {
+    res.status(404).render("./other/notFound") 
+  })
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('./error');
+});
+
 module.exports = app;
